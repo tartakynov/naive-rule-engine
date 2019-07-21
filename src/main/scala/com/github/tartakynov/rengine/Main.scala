dea004f57cc3e823db48c13efc7d0369cce80134
Artem Tartakynov <artem.tad@gmail.com>
2019-07-21 22:18:08 +0700

    finalize parser

--- src/main/scala/com/github/tartakynov/rengine/Main.scala
+++ src/main/scala/com/github/tartakynov/rengine/Main.scala
@@ -5,7 +5,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val parser = new RuleParser()
-    val expression = parser.parse("A >= -1 AND A <= 5")
+    val expression = parser.parse("A(F, C(1)) = 1")
     System.out.println(expression.toString)
   }
 }
